d604a6b43ad869f55a4ccda2e55586a932aef182
Matthew Ko <iammatthewko114@gmail.com>
2023-03-16 21:43:14 +0800

    read input file

--- reduce.cpp
+++ reduce.cpp
@@ -5,21 +5,30 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-    cout <<"Value of argc: " << argc << "\n";
-    cout <<"Value of argv: " << *argv << "\n";
-    const char* in_file = "./gen0_in.txt";
+    //argc: no. of arguments
+    //argv: segments of the command: 0: call file, 1: arg1, 2: arg2, 3: arg3
+    cout <<"Input file:  " << argv[1]  << "\n";
+    cout <<"Output file: " << argv[2]  << "\n";
+    const char* in_file = argv[1];
     ifstream in_file_stream;
     in_file_stream.open(in_file, ios::in);
-    const char* out_file = "./gen0_out.txt";
+    const char* out_file = argv[2];
     ifstream out_file_stream;
     out_file_stream.open(out_file, ios::out);
     if (argc < 3 || argc > 4){
-        cout << "usage: reduce " << in_file << " " << out_file << "\n";
+        cout << "Reduce function error: incorrect numbers of arguments\n";
         exit(1);
     }
     char *inFile = argv[1];
     char *outFile = argv[2];
     char *logFile = (argc == 4 ? argv[3] : NULL);
+
+    //read from file
+    while(!in_file_stream.eof()){
+        string value;
+        getline(in_file_stream, value);
+        cout << value << "\n";
+    }
     in_file_stream.close();
     out_file_stream.close();
     return 0;
